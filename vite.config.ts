89a162b116012d3b636c97af174e3408e8c769f1
Carlos <contato.carlosborges@hotmail.com>
2025-12-11 10:36:01 -0300

    Adicao de imagens

--- vite.config.ts
+++ vite.config.ts
@@ -3,22 +3,19 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-    fs: {
-      allow: [".","./client", "./shared"],
-      deny: [".env", ".env.*", "*.{crt,pem}", "**/.git/**"],
-    },
-  },
   build: {
     outDir: "dist/spa",
+    emptyOutDir: true,
+    // Garante que assets sejam copiados
+    assetsInclude: ['**/*.jpeg', '**/*.jpg', '**/*.png', '**/*.gif', '**/*.svg']
+  
   },
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./client"),
       "@shared": path.resolve(__dirname, "./shared"),
+      "@assets": path.resolve(__dirname, "./client/src"),
     },
   },
 }));
